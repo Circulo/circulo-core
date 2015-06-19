fc79b8e2b308af6405e9e1d979cecd21211737c9
K. M. Fazle Azim Babu <azim.babu@gmail.com>
2015-06-19 00:16:11 -0700

    Adding more tests for product repository

--- src/main/java/com/circulo/model/AssemblyItem.java
+++ src/main/java/com/circulo/model/AssemblyItem.java
@@ -7,37 +7,28 @@ import java.math.BigDecimal;
  */
 public class AssemblyItem {
 
-    private String id;
-
-    // Scenario 1: a procurement item may be divided into one or more assembly items. In that case, keeping a single procurement item here suffices.
-    // Scenario 2 : One ore more procurement items having the same sku, may be combined into one assembly item. In that case, keeping a list of procurement items may be needed.
-    // But how to make sure the list consists of same skus??
-    //@TODO : Confirm with Todd whether it should contain a list or a single procurement item.
-    // For the time being keeping a single procuremnt item reference here covering scenario 1.
-    String procurementItemId;
+    private String sku;
 
     private Integer count;
 
-    private BigDecimal unitCost;
-
-    private BigDecimal tax;
+    private String unitOfMeasure;
 
     private String notes;
 
-    public String getId() {
-        return id;
+    public String getSku() {
+        return sku;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setSku(String sku) {
+        this.sku = sku;
     }
 
-    public String getProcurementItemId() {
-        return procurementItemId;
+    public String getUnitOfMeasure() {
+        return unitOfMeasure;
     }
 
-    public void setProcurementItemId(String procurementItemId) {
-        this.procurementItemId = procurementItemId;
+    public void setUnitOfMeasure(String unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
     }
 
     public Integer getCount() {
@@ -46,22 +37,6 @@ public class AssemblyItem {
 
     public void setCount(Integer count) {
         this.count = count;
-    }
-
-    public BigDecimal getUnitCost() {
-        return unitCost;
-    }
-
-    public void setUnitCost(BigDecimal unitCost) {
-        this.unitCost = unitCost;
-    }
-
-    public BigDecimal getTax() {
-        return tax;
-    }
-
-    public void setTax(BigDecimal tax) {
-        this.tax = tax;
     }
 
     public String getNotes() {
